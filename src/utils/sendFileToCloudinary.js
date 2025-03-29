bf74b66fc32020d4d48fdfea6b6045343c571ca6
Andrew <andrejpopkov565@gmail.com>
2025-03-29 10:13:50 +0200

    fix env variables

--- src/utils/sendFileToCloudinary.js
+++ src/utils/sendFileToCloudinary.js
@@ -1,17 +1,14 @@
 import cloudinary from 'cloudinary';
-import { CLOUDINARY } from '../constants/index.js';
 import { getEnvVar } from './getEnvVar.js';
 
 cloudinary.v2.config({
   secure: true,
-  cloud_name: getEnvVar(CLOUDINARY.CLOUD_NAME),
-  api_key: getEnvVar(CLOUDINARY.API_KEY),
-  api_secret: getEnvVar(CLOUDINARY.API_SECRET),
+  cloud_name: getEnvVar('CLOUD_NAME'),
+  api_key: getEnvVar('API_KEY'),
+  api_secret: getEnvVar('API_SECRET'),
 });
 
 export const sendFileToCloudinary = async (file) => {
   const response = await cloudinary.v2.uploader.upload(file.path);
-  console.log(response.secure_url);
-
   return response.secure_url;
 };
